8b2856bcbef6874d3db258f97da6c2f881522c79
Nemozy <nemozy@list.ru>
2018-01-10 23:14:37 +0300

    Tools
    Test data tools.

---
+++ DDTools/CarsData/CarsData/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarsData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SaveData();
+        }
+
+        private void SaveData()
+        {
+            CarData data = new CarData();
+            data.Model = "FireGTO";
+            data.EnginePower = 2.8f;
+            data.DriveUnit = 0;
+            data.GasolineMax = 20;
+            data.MaxSpeed = 160;
+            data.GasolineConsumption = 18;
+            data.Health = 100;
+            data.Weight = 1540;
+
+            data.Cost = 0;
+            var item = new Types.Tuple<string, List<CarItemData>>("EnginePower", new List<CarItemData>());
+            item.second.Add(new CarItemData() { Model = "EnginePower", Level = 1, Cost = 0, EnginePower = 0, DriveUnit = 0, GasolineMax = 0, MaxSpeed = 1, GasolineConsumption = 0.1f, Health = 0, Weight = 10 });
+            item.second.Add(new CarItemData() { Model = "EnginePower", Level = 2, Cost = 10, EnginePower = 0.05f, DriveUnit = 0, GasolineMax = 0, MaxSpeed = 1, GasolineConsumption = 0.1f, Health = 0, Weight = 10 });
+            item.second.Add(new CarItemData() { Model = "EnginePower", Level = 3, Cost = 30, EnginePower = 0.1f, DriveUnit = 0, GasolineMax = 0, MaxSpeed = 1, GasolineConsumption = 0.2f, Health = 0, Weight = 10 });
+            data.Items.Add(item);
+
+            Serializator.SaveFile(data, @"F:/aatestf");
+        }
+    }
+}
---
+++ DDTools/CarsData/CarsData/Serialization/Serializator.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+public class Serializator
+{
+    public static BinaryFormatter bf = new BinaryFormatter();
+
+    public static void SaveFile(object serializableObject, string path)
+    {
+        FileStream file = File.Open(path + ".dtd", FileMode.Create);
+        bf.Serialize(file, serializableObject);
+        ////byte[] barr = new UTF8Encoding(true).GetBytes(dataasstring);
+        ////file.Write(barr, 0, barr.Length);
+        StreamWriter writer = new StreamWriter(file);
+        writer.WriteLine(Encode(serializableObject));
+        file.Close();
+    }
+
+    public static string Encode(object serializableObject)
+    {
+        MemoryStream memoryStream = new MemoryStream();
+        bf.Serialize(memoryStream, serializableObject);
+        string tmp = System.Convert.ToBase64String(memoryStream.ToArray());
+        return tmp;
+    }
+
+    public static T Decode<T>(string serialized)
+    {
+        MemoryStream dataStream = new MemoryStream(System.Convert.FromBase64String(serialized));
+        T deserializedObject = (T)bf.Deserialize(dataStream);
+        return deserializedObject;
+    }
+}
